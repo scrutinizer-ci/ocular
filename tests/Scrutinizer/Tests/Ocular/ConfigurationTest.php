01fe7d840b02bf37387424bfc6cc9ceb23acc8b7
Clemens Sahs <clemens.sahs@slide-worker.org>
2014-04-29 17:10:33 +0200

    add Configuration tests

---
+++ tests/Scrutinizer/Tests/Ocular/ConfigurationTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Scrutinizer\Tests\Ocular;
+
+use Scrutinizer\Ocular\Configuration;
+
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     * @dataProvider providerTestConfiguration
+     */
+    public function testConfiguration($accessToken, $instance)
+    {
+        $configuration = new Configuration($accessToken);
+
+
+        $token = $configuration->getAccessToken();
+
+        $this->assertInstanceOf($instance, $token);
+        if (!is_null($accessToken)) {
+            $this->assertEquals($accessToken, $token->get());
+        }
+    }
+
+    public function providerTestConfiguration()
+    {
+        return array(
+            array('accesss key for', '\PhpOption\Some'),
+            array(null, '\PhpOption\None'),
+        );
+    }
+
+}
