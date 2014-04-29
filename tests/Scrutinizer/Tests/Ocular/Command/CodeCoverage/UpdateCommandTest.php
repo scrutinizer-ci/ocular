008317626d62810c95b340a1c56e90aa9d9fd9d7
Clemens Sahs <clemens.sahs@slide-worker.org>
2014-04-29 17:10:55 +0200

    add blank tests

---
+++ tests/Scrutinizer/Tests/Ocular/Command/CodeCoverage/UpdateCommandTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Scrutinizer\Tests\Ocular\Command\CodeCoverage;
+
+class UpdateCommandTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testExecute()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGeneratePostData()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetCoverageData()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetBasePath()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testParseFormat()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testParseRepositoryName()
+    {
+        $this->markTestIncomplete();
+    }
+    public function testParseParents()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testParseRevision()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testConfigure()
+    {
+        $this->markTestIncomplete();
+    }
+
+}
---
+++ tests/Scrutinizer/Tests/Ocular/Command/SelfUpdateCommandTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Scrutinizer\Tests\Ocular\Command;
+
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testExecuteWithNonCompiledVersion()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testExecuteWithOfflineRevisionValue()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testExecuteUp2Date()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testExecuteCanNotDownload()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testExecuteCanNotDeployNewVersion()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testExecuteWorkFine()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testConfigure()
+    {
+        $this->markTestIncomplete();
+    }
+}
